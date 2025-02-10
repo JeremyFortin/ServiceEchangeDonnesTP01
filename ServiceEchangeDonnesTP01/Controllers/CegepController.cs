7a25fb5510b4c30e50133b0f105facbe9f2fa690
jérémy Fortin <2142667@cegeprdl.ca>
2025-02-10 02:09:51 -0500

    Add project files.

---
+++ ServiceEchangeDonnesTP01/Controllers/CegepController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceEchangeDonnesTP01.Modeles;
+using ServiceEchangeDonnesTP01.DTOs;
+using ServiceEchangeDonnesTP01.Controleurs;
+
+namespace ServiceEchangeDonnesTP01.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CegepController : ControllerBase
+    {
+
+
+
+        [HttpGet]
+        public JsonResult GetCegep(string nom)
+        {
+            CegepDTO cegep = CegepControleur.Instance.ObtenirCegep(nom);
+            if (cegep == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(cegep));
+        }
+
+        [HttpPost]
+        public JsonResult AddCegep(CegepDTO cegep)
+        {
+
+            CegepControleur.Instance.AjouterCegep(cegep);
+            return new JsonResult(Ok(cegep));
+
+
+        }
+
+        [HttpPost]
+        public JsonResult EditCegep(CegepDTO cegep)
+        {
+
+            if (CegepControleur.Instance.ObtenirCegep(cegep.Nom) !=null) 
+            {
+                CegepControleur.Instance.ModifierCegep(cegep);
+                return new JsonResult(Ok(cegep));
+            }
+                return new JsonResult(NotFound());
+
+            
+        }
+
+        [HttpGet]
+        public JsonResult GetAllCegep()
+        {
+            List<CegepDTO> cegeps = CegepControleur.Instance.ObtenirListeCegep();
+            if (cegeps == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(cegeps));
+        }
+
+        [HttpDelete]
+        public JsonResult DeleteCegep(string nom)
+        {
+            CegepDTO cegep = CegepControleur.Instance.ObtenirCegep(nom);
+            if (cegep == null)
+                return new JsonResult(NotFound());
+
+            CegepControleur.Instance.SupprimerCegep(nom);
+            return new JsonResult(NoContent());
+        }
+
+        [HttpDelete]
+        public JsonResult DeleteAllCegep()
+        {
+            CegepControleur.Instance.ViderListeCegep();
+            return new JsonResult(NoContent());
+        }
+    }
+}
